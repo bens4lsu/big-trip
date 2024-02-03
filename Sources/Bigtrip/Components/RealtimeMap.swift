9fded203bc01d53794fae8ad2502d957e6388637
Ben Schultz <bens4lsu@gmail.com>
2024-02-03 19:24:41 +0100

    link to predictwind as alt to real time map

--- Sources/Bigtrip/Components/RealtimeMap.swift
+++ Sources/Bigtrip/Components/RealtimeMap.swift
@@ -17,7 +17,7 @@ struct RealtimeMap: Component {
             Paragraph("Placeholder for real-time tracking.  Some lines are on the map from previous voyages of a previous Velvet Elvis.")
             Paragraph{
                 Text("If you have trouble navigating the map in this frame")
-                Link("go directly to the page", url: EnvironmentKey.predictWindSrc)
+                Link(" go directly to the page", url: EnvironmentKey.predictWindSrc)
                 Text(".")
             }
             IFrame(url: EnvironmentKey.predictWindSrc, addBorder: true, allowFullScreen: true, enabledFeatureNames: []).class("iframe-tracking")
